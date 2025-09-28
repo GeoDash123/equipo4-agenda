517b21037c22ea4921930a9982074d7009fde9fa
José Carlos <110804581+GeoDash123@users.noreply.github.com>
2025-09-27 22:50:00 -0700

    Delete direccion.py

--- direccion.py
+++
@@ -1,6 +0,0 @@
-class Direccion:
-    def __init__(self, direccion_id: int, calle: str, ciudad: str, estado: str):
-        self.direccion_id = direccion_id
-        self.calle = calle
-        self.ciudad = ciudad
-        self.estado = estado
